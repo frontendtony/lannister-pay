e0c21991ed19abf6b0d19e5f4b3db4aaf6372cb4
Anthony Oyathelemhi <tonero91@gmail.com>
2022-02-28 19:34:30 +0100

    resoolve db connection on prod(heroku)

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,20 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const client = new Sequelize(process.env.DATABASE_URL, { dialect: 'postgres', ssl: true });
+const useSSL = ['production', 'staging'].includes(
+  (process.env.NODE_ENV || 'development').toLowerCase()
+);
+
+const client = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: useSSL
+      ? {
+          require: true,
+          rejectUnauthorized: false,
+        }
+      : false,
+  },
+});
 
 exports.client = client;
