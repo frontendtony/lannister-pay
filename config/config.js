eeefdd261c0b715b2d7dfbad57990acba347d0cc
Anthony Oyathelemhi <tonero91@gmail.com>
2022-02-28 19:50:22 +0100

    one last attempt!!!

--- config/config.js
+++ config/config.js
@@ -2,13 +2,28 @@ module.exports = {
   development: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
