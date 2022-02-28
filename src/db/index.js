43e9646ff8b01eabea8f3d16f1244a7df88990f2
Anthony Oyathelemhi <tonero91@gmail.com>
2022-02-28 05:51:25 +0100

    connect to db over ssl

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const client = new Sequelize(process.env.DATABASE_URL, { dialect: 'postgres' });
+const client = new Sequelize(process.env.DATABASE_URL, { dialect: 'postgres', ssl: true });
 
 exports.client = client;
